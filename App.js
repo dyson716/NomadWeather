a85324ec149a972001e4fb446dba55b74721942b
dyson716 <dyson716@daum.net>
2025-01-24 16:48:24 +0900

    Get Weather Info Test

--- App.js
+++ App.js
@@ -1,42 +1,120 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
+
+// const SCREEN_WIDTH = Dimensions.get("window").width;
+const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
-  return (
-    /*
-    <View style={styles.container}>
-      <Text style={textStyles.container}>Hello! I made a RN APP !</Text>
-      <Text style={styles.text}>Hello! I made a RN APP !</Text>
-      <Text style={{ color: "blue", fontSize: 30 }}>Hello! I made a RN APP !</Text>
-      <StatusBar style="auto" />
-    </View>
-    */
+  /*
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 1.2, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting...";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  */
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState(null);
+  const [ok, setOk] = useState(true);
+  const ask = async() => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    
+    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude }, 
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView 
+        pagingEnabled
+        horizontal
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>24</Text>
+          <Text style={styles.description}>Sunny</Text>
+          {/* <Text>{text}</Text> */}
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>25</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>26</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>28</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-// 다양한 스타일 적용법
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "teal",
   },
-  text: {
-    fontSize: 30,
-    color: "#f0f",
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightpink",
+  },
+  cityName: {
+    color: "white",
+    fontSize: 80,
+  },
+  weather: {
+    backgroundColor: "lightyellow",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 80,
+  },
+  description: {
+    fontSize: 40,
   }
 });
-
-const textStyles = {
-  container: {
-    fontSize: 28,
-    color: "green",
-  }
-}
