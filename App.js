7a0959379ab9bd97ad8eeb3a6f4a7147840f813b
dyson716 <dyson716@daum.net>
2025-01-24 14:53:57 +0900

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={textStyles.container}>Hello! I made a RN APP !</Text>
+      <Text style={styles.text}>Hello! I made a RN APP !</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 30,
+    color: "#f0f",
+  }
 });
+
+const textStyles = {
+  container: {
+    fontSize: 28,
+    color: "green",
+  }
+}
